2f7bdc0b8eea1ad28bcb184c44cd735aa96f00e4
steve5631 <alen2048@outlook.com>
2019-07-11 16:42:42 +0800

    Use api_service and jwt_service replace interceptor

---
+++ src/util/api_service.js
@@ -0,0 +1,47 @@
+const API_URL = "http://localhost:9090";
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import JwtService from "@/util/jwt_service";
+
+const ApiService = {
+  init() {
+    Vue.use(VueAxios, axios);
+    Vue.axios.defaults.baseURL = API_URL;
+  },
+  setHeader() {
+    Vue.axios.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${JwtService.getToken()}`;
+  },
+  query(resource, params) {
+    return Vue.axios.get(resource, params).catch(error => {
+      throw new Error(`[YunNet] ApiService ${error}`);
+    });
+  },
+  get(resource, slug = "") {
+    return Vue.axios.get(`${resource}/${slug}`).catch(error => {
+      throw new Error(`[YunNet] ApiService ${error}`);
+    });
+  },
+
+  post(resource, params) {
+    return Vue.axios.post(`${resource}`, params);
+  },
+
+  update(resource, slug, params) {
+    return Vue.axios.put(`${resource}/${slug}`, params);
+  },
+
+  put(resource, params) {
+    return Vue.axios.put(`${resource}`, params);
+  },
+
+  delete(resource) {
+    return Vue.axios.delete(resource).catch(error => {
+      throw new Error(`[YunNet] ApiService ${error}`);
+    });
+  }
+};
+
+export default ApiService;
