86e52cbae909e13e5becc3d7641f06bbcb765ade
steve5631 <alen2048@outlook.com>
2019-07-23 21:31:59 +0800

    Vuex Redesign
    add Backend api request
    modular management store

---
+++ src/store/mutations_type.js
@@ -0,0 +1,6 @@
+export const PURGE_AUTH = "logOut";
+export const SET_AUTH = "setUser";
+export const SET_COMMENTS = "setComments";
+export const SET_ERROR = "setError";
+export const SET_PROFILE = "setProfile";
+export const RE_AUTH = "reAuth";
